1e73872274b862b3015bddc7d123dea36f558aaa
tildejustin <spacepepper3.14@gmail.com>
2024-02-23 10:31:08 -0500

    literally text

--- src/main/java/fast_reset/client/client/SaveWorldScreen.java
+++ src/main/java/fast_reset/client/client/SaveWorldScreen.java
@@ -1,20 +1,19 @@
 package fast_reset.client.client;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.*;
 
 import java.awt.*;
 
 public class SaveWorldScreen extends Screen {
-    public SaveWorldScreen(){
+    public SaveWorldScreen() {
         super(new TranslatableText("menu.savingLevel"));
     }
 
-    public void render(int mouseX, int mouseY, float delta){
+    public void render(int mouseX, int mouseY, float delta) {
         this.renderBackground();
 
-        this.drawCenteredString(this.font, new TranslatableText("still saving the last world").asString(), this.width / 2, 70, Color.white.getRGB());
+        this.drawCenteredString(this.font, new LiteralText("still saving the last world").asString(), this.width / 2, 70, Color.white.getRGB());
 
         super.render(mouseX, mouseY, delta);
     }
